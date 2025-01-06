771bb38c09f820d6c675e1ca95fb14837f805390
vpfernanda <fernandavpagano@gmail.com>
2025-01-06 11:15:03 -0300

    Added Validation to Medico fields.

--- api/src/main/java/med/voll/api/dtos/EnderecoDTO.java
+++ api/src/main/java/med/voll/api/dtos/EnderecoDTO.java
@@ -1,9 +1,31 @@
 package med.voll.api.dtos;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.enums.UFBr;
 
-public record EnderecoDTO(String logradouro, String numero, String complemento,
-                          String bairro, String cidade, UFBr uf, String cep) {
+public record EnderecoDTO(
+                          @NotBlank
+                          String logradouro,
+
+                          String numero,
+
+                          String complemento,
+
+                          @NotBlank
+                          String bairro,
+
+                          @NotBlank
+                          String cidade,
+
+                          @NotNull
+                          UFBr uf,
+
+                          @NotBlank
+                          @Pattern(regexp = "\\d{8}")
+                          String cep) {
     //logradouro, número, complemento, bairro, cidade, UF e CEP
 
 }
